8ef47cf074cb585465c41439e377b15f55381bc4
leofltt <flttleonardo@gmail.com>
2024-05-07 11:06:57 +0200

    Initial commit with submodules

---
+++ README.md
@@ -0,0 +1,28 @@
+# Pawrp
+
+## By leofltt
+
+A simple plugin to try out cookiejuce
+
+Generated using [Cookiejuce](github.com/madskjeldgaard/Cookiejuce).
+
+## Building
+
+Configure:
+```bash
+cmake -S . -B build
+```
+
+Build:
+```bash
+cmake --build build
+```
+
+The plugins are now copied to your system.
+
+Run standalone (on MacOS):
+```bash
+./build/Pawrp_artefacts/Standalone/Pawrp.app/Contents/MacOS/Pawrp
+```
+
+
---
+++ source/PluginProcessor.h
@@ -0,0 +1,77 @@
+#pragma once
+
+#include <juce_audio_processors/juce_audio_processors.h>
+
+#if (MSVC)
+#include "ipps.h"
+#endif
+
+#if USING_RUST
+#include "pawrprustlib/lib.h"
+#endif
+
+class PluginProcessor : public juce::AudioProcessor
+
+  , public juce::AudioProcessorValueTreeState::Listener
+
+{
+public:
+    PluginProcessor();
+    ~PluginProcessor() override;
+
+    void prepareToPlay (double sampleRate, int samplesPerBlock) override;
+    void releaseResources() override;
+
+    bool isBusesLayoutSupported (const BusesLayout& layouts) const override;
+
+    void processBlock (juce::AudioBuffer<float>&, juce::MidiBuffer&) override;
+
+    juce::AudioProcessorEditor* createEditor() override;
+    bool hasEditor() const override;
+
+    const juce::String getName() const override;
+
+    bool acceptsMidi() const override;
+    bool producesMidi() const override;
+    bool isMidiEffect() const override;
+    double getTailLengthSeconds() const override;
+
+    int getNumPrograms() override;
+    int getCurrentProgram() override;
+    void setCurrentProgram (int index) override;
+    const juce::String getProgramName (int index) override;
+    void changeProgramName (int index, const juce::String& newName) override;
+
+    void getStateInformation (juce::MemoryBlock& destData) override;
+    void setStateInformation (const void* data, int sizeInBytes) override;
+
+  
+  // Parameters
+    static juce::AudioProcessorValueTreeState::ParameterLayout createParameterLayout()
+    {
+        juce::AudioProcessorValueTreeState::ParameterLayout layout;
+
+        // Default param1
+        layout.add (std::make_unique<juce::AudioParameterInt> ("param1", "Default Param 1", 0, 12, 0));
+
+        // Default param2
+        layout.add (std::make_unique<juce::AudioParameterFloat> ("param2", "Default Param 2", 0.0f, 1.0f, 0.5f));
+
+       // Default param3
+        layout.add (std::make_unique<juce::AudioParameterChoice> ("param3", "Default Param 3", juce::StringArray { "Option 1", "Option 2", "Option 3" }, 0));
+
+        return layout;
+    }
+
+    // Parameter stuff
+    std::atomic<bool> parametersNeedUpdating;
+    juce::AudioProcessorValueTreeState parameters { *this, nullptr, "Parameters", createParameterLayout() };
+    void parameterChanged (const juce::String& parameterID, float newValue) override;
+    void updateParameters();
+
+    juce::AudioProcessorValueTreeState& getValueTreeState() { return parameters; }
+
+
+private:
+    JUCE_DECLARE_NON_COPYABLE_WITH_LEAK_DETECTOR (PluginProcessor)
+};
